47e8c77d5230e45baf8884568a3c3f13982c657b
ianangelo <ianangelo@localhost>
2020-08-13 21:03:24 +0800

    Swagger2 Config

--- yelp/src/main/java/org/itappa/yelp/config/Swagger2Config.java
+++ yelp/src/main/java/org/itappa/yelp/config/Swagger2Config.java
@@ -24,8 +24,7 @@ public class Swagger2Config {
     }
     private ApiInfo apiEndPointsInfo() {
         return new ApiInfoBuilder().title("Spring Boot REST API")
-                .description("Satellite REST API")
-                .contact(new Contact("Headley Williamson", "www.archinsurance.com", "hwilliamson@archinsurance.com"))
+                .description("REST API")
                 .license("Apache 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
                 .version("1.0.0")
